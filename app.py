682365906289e43165ca683a28b56799c6d7f573
Alexandr Mirovaev <alexandrmirovaev@MacBook-Air-Alexandr.local>
2025-03-06 22:00:23 +0300

    test2

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def chat():
 if __name__ == "__main__":
     app.run(debug=True)
     if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+        app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)),debug=True)
